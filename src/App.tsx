2b5a3d7f95c06712e8c0b2bebc1fb62a2a72fa24
Irakli Mikava <i.mikava365@gmail.com>
2023-06-09 09:29:07 +0200

    init

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Container} from 'react-bootstrap'
+import { Routes, Route, Navigate } from 'react-router-dom';
+import NewNote from './NewNote.tsx'
+function App() {
+  return (
+    <Container className='my-4'>
+
+    <Routes>
+      <Route path="/" element={<h1>Home</h1>} />
+      <Route path="/new" element={<h1><NewNote/></h1>} />
+      <Route path="/:id">
+        <Route index element={<h1>Show</h1>} />
+        <Route path="edit" element={<h1>Edit</h1>} />
+      </Route>
+      <Route path="*" element={<Navigate to="/" />} />
+    </Routes>
+    </Container>
+  );
+}
+
+export default App;
---
+++ src/NoteForm.tsx
@@ -0,0 +1,29 @@
+import { Form,Row,Stack,Col } from 'react-bootstrap';
+
+const NoteForm = () => {
+  return (
+<Form>
+  <Stack gap={4}>
+    <Row>
+      <Col>
+     < Form.Group controlId="title">
+      <Form.Label>Title</Form.Label>
+                      <Form.Control required/>
+
+         </Form.Group>  
+      </Col>
+         <Col>   
+         < Form.Group controlId="title">
+      <Form.Label>Tags</Form.Label>
+      <Form.Control required/>
+
+     </Form.Group>  
+         
+                     </Col>
+    </Row>
+  </Stack>
+</Form>
+  )
+}
+
+export default NoteForm
